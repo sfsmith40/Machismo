3a6037305657bf4c173e99d6e7b87bd15cb5f1dc
Sean Smith <Smitty@Seans-MacBook-Pro.local>
2014-03-15 23:14:34 -0400

    Initial Commit

---
+++ Machismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Machismo
+//
+//  Created by Sean Smith on 3/15/14.
+//  Copyright (c) 2014 Sean Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Machismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Machismo
+//
+//  Created by Sean Smith on 3/15/14.
+//  Copyright (c) 2014 Sean Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
